1cf87606a14cea7184a1609416e0643a6c3ea2dc
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-14 19:00:24 -0700

    Example timer JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
 console.log("I'm here...")
+
+var timerEL = document.getElementById("timer");
+
+var secondsLeft = 60;
+
+function setTime() {
+  // Sets interval in variable - contine from here
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();
