07ad864f908acb4b09df27316143e882312e414e
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-14 23:37:57 -0700

    Added Timer Code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,16 @@ var timerEL = document.getElementById("timer");
 var secondsLeft = 60;
 
 function setTime() {
-  // Sets interval in variable - contine from here
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+    timerEL.textContent = secondsLeft;
 
     if(secondsLeft === 0) {
-      // Stops execution of action at set interval
       clearInterval(timerInterval);
-      // Calls function to create and append image
       sendMessage();
     }
 
   }, 1000);
-}
-
-// Function to create and append colorsplosion image
-function sendMessage() {
-  timeEl.textContent = " ";
-  var imgEl = document.createElement("img");
-  imgEl.setAttribute("src", "images/image_1.jpg");
-  mainEl.appendChild(imgEl);
-
 }
 
 setTime();
