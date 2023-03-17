625e98af774c74344917af819fcf5858ddb0b236
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-16 20:22:17 -0700

    Study Session Massive Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,188 @@
-var questionsArray = [
-  {
-    question: 'Which of the following is not a commonly used data type?', 
-    answers: ["Strings", "Booleans", "Alerts", "Numbers"],
-    correctAnswer: 2
-  },
-  {
-    question: 'Where is the condition in an if/then statement enclosed?',
-    answers: [ "Parentheses", "Quotes", "Curly Brackets", "Square Brackets"],
-    correctAnswer: 0
-  },
-  {
-    question: 'Arrays in JavaScript can be used to store what?',
-    answers: ["Numbers and Stings", "Other Arrays", "Booleans", "All of the Above"],
-    correctAnswer: 3
-  },
-  {
-    question: 'String values must be enclosed within which symbol when assigned to variables?',
-    answers: ["Curly Brackets", "Quotes", "Parentheses", "Commas"],
-    correctAnswer: 1
-  },
-  {
-    question: 'What is a very useful tool for printing content to the debugger during development?',
-    answers: ["JavaScript", "Terminal/Bash", "For Loops", "Console Log"],
-    correctAnswer: 3
-  }
-]
+var startPage = document.querySelector('#startpage');
+var quizCard = document.querySelector ('#quizcard');
+var questionCard = document.querySelector ("#questioncard");
+var answerCard = document.querySelector ("#answercard");
+var endPage = document.querySelector ("#end");
+var timerElement = document.querySelector ("#secondsLeft");
+var startBttn = document.querySelector("#startQuiz");
+var highScoresBttn = document.querySelector("#highscores");
+var scoreCard = document.querySelector("#scorecard")
+var timerScoreBar =  document.querySelector("#tm-sc-bar")
+var finalScore = document.querySelector("#final-score");
+var form = document.querySelector("#enterScore")
+var initials = document.querySelector("#initials")
+var result ;
+var footer = document.querySelector("footer")
+var answerBttns = document.querySelector(".answers")
+var score;
+var storedScores =[];
+var timer;
+var timerCount;
+var currentQuestionIndex = 0;
 
-var startBttn = document.getElementById('start-bttn');
-var questionCard = document.getElementById('q-card');
-var questionEL = document.getElementById('question');
-var answerBttnEL = document.getElementById('answer-bttn');
-var startCardEL = document.getElementById('startcard');
-var endCardEL = document.getElementById('endcard'); 
+const questionsArray = [
+    {
+      question: 'Which of the following is not a commonly used data type?', 
+      answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+      correctAnswer: 2
+    },
+    {
+      question: 'Where is the condition in an if/then statement enclosed?',
+      answers: [ "Parentheses", "Quotes", "Curly Brackets", "Square Brackets"],
+      correctAnswer: 0
+    },
+    {
+      question: 'Arrays in JavaScript can be used to store what?',
+      answers: ["Numbers and Stings", "Other Arrays", "Booleans", "All of the Above"],
+      correctAnswer: 3
+    },
+    {
+      question: 'String values must be enclosed within which symbol when assigned to variables?',
+      answers: ["Curly Brackets", "Quotes", "Parentheses", "Commas"],
+      correctAnswer: 1
+    },
+    {
+      question: 'What is a very useful tool for printing content to the debugger during development?',
+      answers: ["JavaScript", "Terminal/Bash", "For Loops", "Console Log"],
+      correctAnswer: 3
+    }
+  ];
 
-startBttn.addEventListener('click', startQuiz);
 
-function startQuiz() {
-  console.log('Funkytown');
-  startBttn.classList.add('hide');
-  startCardEL.classList.add('hide');
-  questionCard.classList.remove('hide');
+function startGame () {
+    currentQuestionIndex = 0;
+    console.log("Funkytown")
+    startPage.setAttribute ("style", "display:none;");
+    highScoresBttn.setAttribute("style", "visibility:hidden;");
+    timerScoreBar.setAttribute("style", "display:flex;")
+    timerCount = (questionsArray.length * 12) + 1;
+
+    startTimer()
+    createQuiz()
 }
 
 
 
-// function nextQuestion() {
-//   showQuestion(currentQuestion);
-
-// }
-
-// function showQuestion(questionsArray) {
-//   questionEL.innertext = questionsArray.question;
-//   questionsArray.answers.array.forEach(element => {
-//     const button = document.createElement('button');
-//     button.innerText = answer.text;
-//     button.classList.add('bttn');
-//     if (answer.correct) {
-//       button.dataset.correct = answer.correct;
-//     }
-//     button.addEventListener('click', selectAnswer);
-//     answerBttnEL.appendChild(button);
-//   });
-
-// }
-
-// function selectAnswer() {
-
-// }
+function startTimer () {
+    console.log('Studio 54')
+    timerInterval = setInterval(function() {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount === 0) {
+            gameOver();
+        }
+    }, 1000);
+}
 
 
+function createQuiz () {
+    console.log('I love the night life.')
+    if (currentQuestionIndex < questionsArray.length) {
+        quizCard.setAttribute("style", "display:contents;");
+        questionCard.setAttribute("style", "display:contents;");
+        answerCard.setAttribute("style", "display:flex;");
+        var currentQuestion = questionsArray[currentQuestionIndex];
+        questionCard.innerHTML = currentQuestion.question;
+        var numberOfAnswers = currentQuestion.answers.length
+        answerCard.textContent = "";
+        for (var i = 0; i < numberOfAnswers; i++) {
+            var choice = currentQuestion.answers[i];
+            var answerChoices = document.createElement("button");
+            answerChoices.textContent = choice;
+            answerChoices.setAttribute("class", "answers");
+            answerChoices.setAttribute("data-index", i);
+            answerChoices.addEventListener("click", answerResult);
+            answerCard.appendChild(answerChoices);
+        }
+    } else {
+        finishGame();
+    }
+}
 
-// // console.log("I'm here...")
+function resultPrompt () {
+    console.log("Don't leave me this way");
+    if (result) {
+        footer.setAttribute("display", "contents");
+        footer.innerHTML = "Right-o!";
+        setTimeout(function() {
+            footer.innerHTML = "";
+        },4000);
+    } else  {
+        footer.setAttribute("display", "contents");
+        footer.innerHTML = "Nope!";
+        setTimeout(function() {
+            footer.innerHTML = "";
+        },4000);
+    }
 
-// var startBttn = document.getElementById("start-button");
-// var timerEL = document.getElementById("timer");
-// var secondsLeft = 60;
+}
 
-// startBttn.addEventlistener("click", setTime());
+function answerResult (event) {
+    console.log('Rock the boat.')
+    var chosenAnswer = event.target.dataset.index;
+    if (chosenAnswer == questionsArray[currentQuestionIndex].correctAnswer) {
+        result = true;
+        resultPrompt();
+    } else {
+        result = false
+        timerCount -= 10;
+        console.log(result)
+        resultPrompt();
+    }
+    currentQuestionIndex++;
+    createQuiz()
 
-// function setTime() {
-//   console.log("If I could turn back time");
-//   var timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timerEL.textContent = secondsLeft;
+}
 
-//     if(secondsLeft === 0) {
-//       clearInterval(timerInterval);
-//       sendMessage();
-//     }
+function finishGame () {
+    clearInterval(timerInterval);
+    score = timerCount;
+    timerElement.textContent = score;
+    quizCard.innerHTML = "All Done.";
+    endPage.setAttribute("style", "display:contents;");
+    finalScore.textContent = score;
+    form.addEventListener("submit", function(event) {
+        quizCard.innerHTML = ""
+        event.preventDefault();
+        enterScore();
+        scoresTable()
+    })
+}
 
-//   }, 1000);
-// };
+function enterScore () {
+    var inputInitials = initials.value.toUpperCase();
+    var playerScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    var highScores = {
+        initials: inputInitials,
+        score: score
+    };
+    playerScores.push(highScores);
+    localStorage.setItem("highScores", JSON.stringify(playerScores));
+    scoresTable();
+}
+
+function scoresTable () {
+    startPage.setAttribute ("style", "display:none;");
+    quizCard.setAttribute ("style", "display:none;");
+    endPage.setAttribute("style", "display:none;");
+    scoreCard.setAttribute ("style", "display:flex;");
+    timerScoreBar.setAttribute("style", "display:none;");
+    var playerHighScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    scoreCard.innerHTML="";
+    for (i=0; i<playerHighScores.length; i++) {
+        var listItemEl = document.createElement("li");
+        listItemEl.textContent = playerHighScores[i].initials + "  -  " + playerHighScores[i].score
+        scoreCard.appendChild(listItemEl)
+    }
+}
+
+function gameOver () {
+    clearInterval(timerInterval)
+    answerCard.setAttribute("style", "display:none;")
+    questionCard.innerHTML = "Time's Up!"
+    highScoresBttn.setAttribute("style", "visibility:visible;")
+}
+
+startBttn.addEventListener("click", startGame)
+
+highScoresBttn.addEventListener("click", scoresTable)
